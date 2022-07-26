8be5bb0ebf17f067822ffa9b2b4b9207501a8e64
Frank Mayer <frank.mayer1907@protonmail.com>
2022-07-26 21:05:32 +0200

    "new Array()" -> "[]"

--- src/model/CharacterSheet.ts
+++ src/model/CharacterSheet.ts
@@ -305,7 +305,7 @@ export class CharacterSheetModel {
     this.ryo = data.ryo ?? 100;
     this.equipment = data.equipment
       ? data.equipment.map((x) => new Equipment(x))
-      : new Array();
+      : [];
 
     this.armorClass = data.armorClass ?? 0;
     this.initiative = data.initiative ?? 0;
