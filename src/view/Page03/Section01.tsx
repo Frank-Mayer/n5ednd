31e4bc45fcdd3e4373b0259cd4264f4a88baf345
Frank Mayer <frank.mayer1907@protonmail.com>
2022-07-29 02:53:24 +0200

    fixed save dc label

--- src/view/Page03/Section01.tsx
+++ src/view/Page03/Section01.tsx
@@ -6,8 +6,20 @@ export const Section01 = () => (
     <PrimitiveView index="ninjutsuAttackBonus" style="underline" />
     <PrimitiveView index="taijutsuAttackBonus" style="underline" />
     <PrimitiveView index="genjutsuAttackBonus" style="underline" />
-    <PrimitiveView index="ninjutsuSaveDC" style="underline" />
-    <PrimitiveView index="taijutsuSaveDC" style="underline" />
-    <PrimitiveView index="genjutsuSaveDC" style="underline" />
+    <PrimitiveView
+      index="ninjutsuSaveDC"
+      label="Ninjutsu Save DC"
+      style="underline"
+    />
+    <PrimitiveView
+      index="taijutsuSaveDC"
+      label="Taijutsu Save DC"
+      style="underline"
+    />
+    <PrimitiveView
+      index="genjutsuSaveDC"
+      label="Genjutsu Save DC"
+      style="underline"
+    />
   </section>
 );
